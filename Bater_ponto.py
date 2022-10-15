16291296b4a0635826889884ebbc708e53f5e804
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-10-15 16:22:56 -0300

    Update Bater_ponto.py
    2º DIa tentando automatizar o ponto kk

--- Bater_ponto.py
+++ Bater_ponto.py
@@ -1,16 +1,19 @@
-#Programinha para fins de estudos, automatizar a tarefa de bater ponto online
-#Já consigo treinar Python e WebScrapping na mesma paulada
-#Inicio: 14/10/22 (faz input nos dois campos)
-
-
+from argparse import Action
+from selenium.webdriver.common.keys import Keys
+from ssl import Options
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
+import time
 
 #Instalando driver do browser
-service = EdgeService(executable_path=EdgeChromiumDriverManager().install())
-driver = webdriver.Edge(service=service)
+service = FirefoxService(executable_path=GeckoDriverManager().install())
+options = FirefoxOptions()
+options.set_preference("dom.push.enabled", False); options.set_preference("dom.webnotifications.enabled", False)
+options.set_preference('javascript.enabled', True) #se ficar false o site n da a confirmacao
+driver = webdriver.Firefox(options=options, service=service)
 
 #Requisitando a URL desejada
 driver.get("https://app.tangerino.com.br/Tangerino/pages/baterPonto")
@@ -23,6 +26,12 @@ employee_passwd = str("1341")
 employee_box = driver.find_element(By.ID, 'codigoEmpregador').send_keys(employee_code)
 employee_box2 = driver.find_element(By.ID, 'codigoPin').send_keys(employee_passwd)
 
-#clicando na confirmação
-confirm_box = driver.find_element(By.CLASS_NAME, 'btnLogin ladda-button').click
+
+driver.find_element(By.ID, 'codigoPin').send_keys(Keys.TAB)
+
+driver.find_element(By.ID, 'registraPonto').click
+
+#driver.find_element(By.ID, 'registraPonto').send_keys(Keys.ENTER)
+
+
 
