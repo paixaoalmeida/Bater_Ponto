4e7bbe5eceac6457b4165d548d329af78deb3756
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-10-14 23:22:59 -0300

    Update Bater_ponto.py
    Treinando Python e WebScrapping

--- Bater_ponto.py
+++ Bater_ponto.py
@@ -1,14 +1,28 @@
+#Programinha para fins de estudos, automatizar a tarefa de bater ponto online
+#Já consigo treinar Python e WebScrapping na mesma paulada
+#Inicio: 14/10/22 (faz input nos dois campos)
+
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Edge(executable_path=r'C:\Users\pc\Downloads\edgedriver_win64\msedgedriver.exe')
+#Instalando driver do browser
+service = EdgeService(executable_path=EdgeChromiumDriverManager().install())
+driver = webdriver.Edge(service=service)
 
-driver.get("https://app.tangerino.com.br/Tangerino/pages/baterPonto/")
+#Requisitando a URL desejada
+driver.get("https://app.tangerino.com.br/Tangerino/pages/baterPonto")
 
-codigo_emprega = 'PWDIU'
-pin_code = str(6462)
+#Variaveis para registrar o ponto (codigo empregador e senha do funcionario)
+employee_code = 'woooo'
+employee_passwd = str("1341")
 
-driver.find_element(By.ID("codigoEmpregador")).send_keys("ola")
-driver.find_element(By.ID("codigoPin")).send_keys("123")
+#Input na página
+employee_box = driver.find_element(By.ID, 'codigoEmpregador').send_keys(employee_code)
+employee_box2 = driver.find_element(By.ID, 'codigoPin').send_keys(employee_passwd)
+
+#clicando na confirmação
+confirm_box = driver.find_element(By.CLASS_NAME, 'btnLogin ladda-button').click
+
